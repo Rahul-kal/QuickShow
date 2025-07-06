34e6bb3e9924b7621e8014550a40a3eb36bfa4e1
Rahul-kal <rahulkaliraman720@gmail.com>
2025-07-06 23:20:59 +0530

    5 hr server deployment

--- server/server.js
+++ server/server.js
@@ -17,8 +17,27 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
+// ✅ CSP Headers (fixes Google Fonts + unsafe-eval issues)
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com;"
+  );
+  next();
+});
+
+
 // API Routes
-app.get('/', (req, res) => res.send('Server is Live!'));
+//app.get('/', (req, res) => res.send('Server is Live!'));
+app.get('/', (req, res) => {
+  if (req.query.__clerk_handshake) {
+    // Let Clerk handle the handshake
+    res.status(200).send('Clerk handshake acknowledged');
+  } else {
+    res.send('Server is Live!');
+  }
+});
+
 app.use('/api/inngest', serve({ client: inngest, functions }));
 
 // Start server
