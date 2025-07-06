2109c5c8cba5ee75bac050a7e738c1bb453b61f7
Rahul-kal <rahulkaliraman720@gmail.com>
2025-07-06 23:13:14 +0530

    5 hr server deployment

--- server/server.js
+++ server/server.js
@@ -17,15 +17,6 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-// ✅ CSP Headers (fixes Google Fonts + unsafe-eval issues)
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com;"
-  );
-  next();
-});
-
 // API Routes
 app.get('/', (req, res) => res.send('Server is Live!'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
